910c0fde3ecd7acc80131dcd9a2fe9ca2a4045d5
faryasir07 <farooquiyasir140@gmail.com>
2025-03-06 13:28:38 +0530

    commit

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -50,5 +51,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
         return report
 
 
+    except Exception as e:
+        raise CustomException(e,sys)
+def load_objects(file_path):
+    try:
+        with open(file_path,'rb')as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
